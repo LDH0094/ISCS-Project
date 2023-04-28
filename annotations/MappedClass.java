fafaaaaf46fe73aa520dbfc3973aa9a67ab22f49
Deokhyun Lee <deok.lee@obf.ateneo.edu>
2023-04-28 17:20:45 +0800

    added annotations, Save, Delete have to be complete

--- annotations/MappedClass.java
+++ annotations/MappedClass.java
@@ -9,7 +9,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface MappedClass {
-
-
     public Class clazz();
 }
--- annotations/Select.java
+++ annotations/Select.java
@@ -8,5 +8,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Retention(RUNTIME)
 @Target(METHOD)
-public @interface CreateTable {
+public @interface Select {
+    public String value();
 }
