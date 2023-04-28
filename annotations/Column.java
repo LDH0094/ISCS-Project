b90ab32b226e60007faf31d6151379ac99a799e2
Deokhyun Lee <deok.lee@obf.ateneo.edu>
2023-04-28 17:23:34 +0800

    complete annotaions

---
+++ annotations/Column.java
@@ -0,0 +1,16 @@
+package annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+@Target(FIELD)
+public @interface Column {
+
+	public String name();
+	public String sqlType();
+	public boolean id() default false;
+}
