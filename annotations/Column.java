a3af1daad993ac2eff550bead8e126b833847242
Deokhyun Lee <deok.lee@obf.ateneo.edu>
2023-04-29 22:28:35 +0800

    Updated Annotations

--- annotations/Column.java
+++ annotations/Column.java
@@ -3,11 +3,11 @@ package annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.*;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Retention(RUNTIME)
-@Target(FIELD)
+@Target({ TYPE, FIELD, TYPE_USE })
 public @interface Column {
 
 	public String name();
--- annotations/Select.java
+++ annotations/Select.java
@@ -3,11 +3,12 @@ package annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.TYPE_USE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 @Retention(RUNTIME)
-@Target(METHOD)
+@Target({ TYPE, FIELD, TYPE_USE, METHOD })
 public @interface Select {
     public String value();
 }
